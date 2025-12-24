a7e2d73cb58f4842d4d8f245bc711a9bd921e119
Mohamed Tlili <hammatlili666@gmail.com>
2025-12-24 17:22:35 +0100

    Update App.tsx
    fix

--- App.tsx
+++ App.tsx
@@ -1,31 +1,35 @@
+import React from 'react'
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
 
-import React from 'react';
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { Providers } from './app/Providers';
-import { ProtectedRoute } from './components/auth/ProtectedRoute';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Dashboard from './pages/Dashboard';
-import SheetView from './pages/SheetView';
+import { Providers } from './app/Providers'
+import { ProtectedRoute } from './components/auth/ProtectedRoute'
+
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
+import SheetView from './pages/SheetView'
 
 const App: React.FC = () => {
   return (
     <Providers>
-      <Router>
+      <HashRouter>
         <Routes>
+          {/* Public routes */}
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          
+
+          {/* Protected routes */}
           <Route element={<ProtectedRoute />}>
             <Route path="/" element={<Dashboard />} />
             <Route path="/sheet/:id" element={<SheetView />} />
           </Route>
 
+          {/* Fallback */}
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
-      </Router>
+      </HashRouter>
     </Providers>
-  );
-};
+  )
+}
 
-export default App;
+export default App
